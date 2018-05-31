aa01e2b7dc2639453a6be1171890c36868a17527
jmorille <jmorille@gmail.com>
2018-05-31 20:56:36 +0200

    Add webpack

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import mavenParser from 'mvn-artifact-name-parser'
-// import mavenUrl from 'mvn-artifact-url'
+import mavenUrl from 'mvn-artifact-url'
 
 const artifact = mavenParser('org.apache.commons:commons-lang3:3.4')
-console.log(artifact)
+mavenUrl(artifact, undefined).then(url => {
+    console.log("Maven Url", url)
+
+})
