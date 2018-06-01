2f6623868ce652bc0f08a882dddc73b7a8b9d40a
MORILLE Jerome <morille.jerome@groupagrica.com>
2018-06-01 17:26:28 +0200

    classifier reference

--- src/index.ts
+++ src/index.ts
@@ -7,35 +7,51 @@ import mavenParser from 'mvn-artifact-name-parser'
 import mavenUrl from 'mvn-artifact-url'
 import mavenFileName from 'mvn-artifact-filename'
 
+// Config
+const repositoryUrl = 'http://nexus.agrica.loc/repository/maven-releases/';
+
+// groupId:artifactId:packaging:classifier:version
+// const artifactId = 'org.apache.commons:commons-lang3:3.4';
+const artifactId = 'fr.agrica.horodatage:hor-front-web:war:exec:0.0.1-M2';
 
 
-const artifact = mavenParser('org.apache.commons:commons-lang3:3.4')
-console.log("Maven artifact", artifact);
+function downloadArtifact(artifactId: string) {
+    //
+    const artifact = mavenParser(artifactId);
+    console.log("Maven artifact", artifact);
 
-mavenUrl(artifact, 'http://nexus.agrica.loc/repository/maven-releases/').then(url => {
-    console.log("Maven Url", url);
-    return fetch (url);
-}).then(res=> {
-    return new Promise((resolve, reject) => {
-        const hash = crypto.createHash('sha1');
-        const hashMd5 = crypto.createHash('md5');
-        const filename = mavenFileName(artifact);
-        const dest = fs.createWriteStream(`./${filename}`);
-        res.body.on('error', err => { reject(err) });
-        res.body.on('data', chunk => hash.update(chunk));
-        res.body.on('data', chunk => hashMd5.update(chunk));
-        dest.on('finish', () => {
-            const sha1  = hash.digest('hex');
-            const md5  = hashMd5.digest('hex');
-            resolve( {filename, sha1, md5});
-        });
-        res.body.pipe(dest);
-        dest.on('error', err => {
-            reject(err);
+    return mavenUrl(artifact, repositoryUrl).then(url => {
+        console.log("Maven Url", url);
+        return fetch(url);
+    }).then(res => {
+        return new Promise((resolve, reject) => {
+            const hash = crypto.createHash('sha1');
+            const hashMd5 = crypto.createHash('md5');
+            const filename = mavenFileName(artifact);
+            const dest = fs.createWriteStream(`./${filename}`);
+            res.body.on('error', err => {
+                reject(err)
+            });
+            res.body.on('data', chunk => hash.update(chunk));
+            res.body.on('data', chunk => hashMd5.update(chunk));
+            dest.on('finish', () => {
+                const sha1 = hash.digest('hex');
+                const md5 = hashMd5.digest('hex');
+                resolve({filename, sha1, md5});
+            });
+            res.body.pipe(dest);
+            dest.on('error', err => {
+                reject(err);
+            });
         });
     });
-}).then(hash=> {
-    console.log(hash)
+
+}
+
+
+downloadArtifact(artifactId).then(hash => {
+    console.log(hash);
+    return hash;
 });
 
 
