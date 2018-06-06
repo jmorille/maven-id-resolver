decb6a75a77478eae0608ddc32083203a15b040f
jmorille <jmorille@gmail.com>
2018-06-06 23:50:30 +0200

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+        library: "mavenIdResolver",
         libraryTarget: 'commonjs2'
     },
     module: {
